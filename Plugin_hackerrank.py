65938d9fe01499c4a7c3288fa275e793a1a90d5e
kaushik94 <kaushik.varanasi1@gmail.com>
2015-04-09 03:42:14 +0530

    adding more flex

--- Plugin_hackerrank.py
+++ Plugin_hackerrank.py
@@ -8,25 +8,18 @@ username = 'kaushik_varanasi'
 password = 'hackerrank'
 url = 'https://www.hackerrank.com/rest/contests/master/challenges/solve-me-first/'
 
-code = """
 
-def solveMeFirst(a,b):
-  return a+b
-
-
-num1 = input()
-num2 = input()
-res = solveMeFirst(num1,num2)
-print res
-
-"""
+lang = {'py':'python', 'java':'java', 'c':'c'}
 
 class CustomBasicAuthHandler(urllib2.HTTPBasicAuthHandler):
-    '''Preemptive basic auth.
+    '''
+    Preemptive basic auth.
 
     Instead of waiting for a 403 to then retry with the credentials,
     send the credentials if the url is handled by any password manager.
-    Note: please use realm=None when calling add_password.'''
+    Note: please use realm=None when calling add_password.
+
+    '''
     def http_request(self, req):
         url = req.get_full_url()
         realm = None
@@ -43,20 +36,44 @@ class CustomBasicAuthHandler(urllib2.HTTPBasicAuthHandler):
 
 class OpenBrowserCommand(sublime_plugin.TextCommand):
    
-   def run(self,edit):
+   def run(self, edit):
+
+      payload, url, code = self.get_params()
+
+      sock = urllib2.Request(url+'compile_tests', urllib.urlencode(payload))
+      resp = urllib2.urlopen(sock)
+      header = resp.read()
+      header_ = JSON.loads(header)
+      id_ =  header_['model']['id']
+      print 'Submission ID:', id_
+      sock = urllib2.Request(url+'compile_tests/'+str(id_)+'?')
+      resp = urllib2.urlopen(sock)
+      print resp.read()
+
+   def get_params(self):
 
       auth_handler = CustomBasicAuthHandler()
       auth_handler.add_password( realm=None, uri=url, user=username, passwd=password)
       opener = urllib2.build_opener(auth_handler)
       urllib2.install_opener(opener)
 
-      payload = {'code':code, 'customtestcase': 'false', 'language':'python'}
-      sock = urllib2.Request(url+'compile_tests', urllib.urlencode(payload))
-      resp = urllib2.urlopen(sock)
-      header = resp.read()
-      header_ = JSON.loads(header)
-      id_ =  header_['model']['id']
-      print id_
-      sock = urllib2.Request(url+'compile_tests/'+str(id_)+'?')
-      resp = urllib2.urlopen(sock)
-      print resp.read()
+      language, code, custom_testcase = self.get_file_info()
+      custom_testcase = None
+      if custom_testcase is None:
+        payload = {'code':code, 'customtestcase': 'false', 'language':language}
+      return payload, url, code
+
+   def get_file_info(self):
+   	  ffname = self.view.file_name()
+   	  file_name = ffname.split('/')[-1]
+   	  extension = file_name.split('.')[-1]
+   	  language = lang[extension]
+
+   	  code, custom_testcase = self.get_code()
+   	  return language, code, custom_testcase
+
+   def get_code(self):
+
+   	  content = self.view.substr(sublime.Region(0, self.view.size()))
+   	  print content
+   	  return content, None
